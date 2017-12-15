556b5717fc88e83670404bf359dd9f3b6dddc4ed
André <andre.kirsch@t-online.de>
2017-12-15 20:50:50 +0100

    terratex hinzugefügt

---
+++ resources/TerraTex-RL-RPG/Lib/User/StartUp/PlayerDisconnection.cs
@@ -0,0 +1,25 @@
+﻿using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+
+namespace TerraTex_RL_RPG.Lib.User.StartUp
+{
+    public class PlayerDisconnection : Script
+    {
+        public PlayerDisconnection()
+        {
+            API.onPlayerDisconnected += OnPlayerDisconnectedEventHandler;
+        }
+
+        public void OnPlayerDisconnectedEventHandler(Client player, string reason)
+        {
+            if (player.hasSyncedData("loggedin") && player.getSyncedData("loggedin"))
+            {
+                player.setSyncedData("loggedin", false);
+                TTRPG.StorePlayerDataThread.StartToStorePlayerData(player);
+
+                API.consoleOutput("Account " + player.name + "(" + player.getSyncedData("ID") + ") left the server: " +
+                                  reason);
+            }
+        }
+    }
+}
