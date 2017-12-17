8624da23eec563d53aeb0309c5e5f761da22765d
André <andre.kirsch@t-online.de>
2017-12-17 14:00:48 +0100

    Start gamingcorerpg

---
+++ resources/gamingcorerpg/Database/Database.cs
@@ -0,0 +1,29 @@
+using System;
+using MySQL.Data.MySqlClient;
+using GTANetworkServer;
+
+class Database : Script {
+	
+	private string server = "";
+	private string database = "";
+	private string loginName = "";
+	private string password = "";
+	
+	private static string connString = "SERVER=" + server + ";DATABASE=" + database + ";UID=" + loginName + ";PASSWORD=" + password + ";";
+	private static MySqlConnection connection;
+	private static MySqlCommand command;
+	private static MySqlDataReader reader;
+	
+	public static MySqlDataReader executeCommand(string command) {
+		connection = new MySqlConnection(connString);
+		command = connection.createCommand();
+		command.CommandText = command;
+		connection.Open();
+		
+		return reader = commandExecuteReader();
+	}
+	
+	public static void endCommand() {
+		connection.Close();
+	}
+}
