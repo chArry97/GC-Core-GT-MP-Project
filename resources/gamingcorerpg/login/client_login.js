a1b51ed6d753a3de40af36f8439e8eb5bf3e4811
André <andre.kirsch@t-online.de>
2017-12-21 18:50:52 +0100

    Ein paar Login Änderungen

--- resources/gamingcorerpg/login/client_login.js
+++ resources/gamingcorerpg/login/client_login.js
@@ -5,15 +5,19 @@ API.onResourceStart.connect(function() {
 	
 	login_browser = API.createCefBrowser(res.Width/2, res.Height/2);
 	API.waitUntilCefBrowserInit(login_browser);
-	API.setCefBrowserPosition(login_browser, res.Width/2, res.Height/2);
+	API.setCefBrowserPosition(login_browser, res.Width/4, res.Height/4);
 	API.loadPageCefBrowser(login_browser, "/web/start.html");
-	API.setBrowserHeadless(true);
+	API.setCefBrowserHeadless(login_browser, true);
 	API.showCursor(true);
 	API.setCanOpenChat(false);
+	API.setHudVisible(false);
+	API.setPlayerInvincible(true);
 });
 
 function close() {
 	API.showCursor(false);
 	API.destroyCefBrowser(login_browser);
 	API.setCanOpenChat(true);
+	API.setHudVisible(true);
+	API.setPlayerInvincible(true);
 }
