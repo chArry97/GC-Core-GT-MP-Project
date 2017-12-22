b3268119452d162591fe4e51021af61ee9f3e9f1
Charry <Charry@astra4052.startdedicated.de>
2017-12-22 19:14:58 +0100

    Database fix

--- resources/gamingcorerpg/Database/Database.cs
+++ resources/gamingcorerpg/Database/Database.cs
@@ -1,5 +1,4 @@
 using System;
-using MySQL.Data.MySqlClient;
 using GrandTheftMultiplayer.Server;
 using GrandTheftMultiplayer.Server.API;
 using GrandTheftMultiplayer.Server.Elements;
@@ -7,31 +6,30 @@ using GrandTheftMultiplayer.Server.Constant;
 using GrandTheftMultiplayer.Server.Managers;
 using GrandTheftMultiplayer.Shared;
 using GrandTheftMultiplayer.Shared.Math;
+using MySql.Data.MySqlClient;
 
-namespace db {
-	class Database{
+class Database{
+	
+	private static string server = "";
+	private static string database = "";
+	private static string loginName = "";
+	private static string password = "";
+	
+	private static string connString = "SERVER=" + server + ";DATABASE=" + database + ";UID=" + loginName + ";PASSWORD=" + password + ";";
+	private static MySqlConnection connection;
+	private static MySqlCommand command;
+	private static MySqlDataReader reader;
+	
+	public static MySqlDataReader executeCommand(string commandText) {
+		connection = new MySqlConnection(connString);
+		command = connection.CreateCommand();
+		command.CommandText = commandText;
+		connection.Open();
 		
-		private string server = "";
-		private string database = "";
-		private string loginName = "";
-		private string password = "";
-		
-		private static string connString = "SERVER=" + server + ";DATABASE=" + database + ";UID=" + loginName + ";PASSWORD=" + password + ";";
-		private static MySqlConnection connection;
-		private static MySqlCommand command;
-		private static MySqlDataReader reader;
-		
-		public static MySqlDataReader executeCommand(string command) {
-			connection = new MySqlConnection(connString);
-			command = connection.createCommand();
-			command.CommandText = command;
-			connection.Open();
-			
-			return reader = command.ExecuteReader();
-		}
-		
-		public static void endCommand() {
-			connection.Close();
-		}
+		return reader = command.ExecuteReader();
+	}
+	
+	public static void endCommand() {
+		connection.Close();
 	}
 }
