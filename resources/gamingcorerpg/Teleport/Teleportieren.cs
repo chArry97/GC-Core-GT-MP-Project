383da03ae7955a1fc1dcdb3ab0d23fc2c4712c69
Unknown <fussballer01@googlemail.com>
2017-12-30 16:57:34 +0100

    TELEPORTER Zu Spieler

---
+++ resources/gamingcorerpg/Teleport/Teleportieren.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System;
+using GrandTheftMultiplayer.Server;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Constant;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Shared;
+using GrandTheftMultiplayer.Shared.Math;
+
+public class Teleportieren : Script
+{
+    [Command("tp", "Usage: /tp *Player*", GreedyArg = true)] // Admin teleportiert sich zum Spieler.
+    public void teleportToPlayer(Client sender, Client target)
+    {
+        API.sendNotificationToPlayer(sender, "~w~Du wurdest zum Spieler Teleportiert.:~r~" + target.name);
+        API.sendNotificationToPlayer(target, "~r~" + sender.name + " ~w~hat sich zu dir Teleportiert!");
+        sender.position = target.position;
+    }
+    [Command("tptome", "Usage: /tptome *Player*", GreedyArg = true)] // Spieler wird zum Admin teleportiert.
+    public void teleportPlayerToMe(Client sender, Client target)
+    {
+        Player player = sender.getData("player");
+        if (player.Account.AdminLvl < 10)
+            return;
+        API.sendNotificationToPlayer(target, "~w~Du Teleportierst den Spieler zu dir:~r~" + sender.name);
+        API.sendNotificationToPlayer(sender, "~r~" + target.name + " ~w~Du wurdest Teleportiert!");
+        target.position = sender.position;
+    }
+}
--- resources/gamingcorerpg/fixcars/cars.cs
+++ resources/gamingcorerpg/fixcars/cars.cs
@@ -6,8 +6,8 @@ using GrandTheftMultiplayer.Server.Constant;
 using GrandTheftMultiplayer.Server.Managers;
 using GrandTheftMultiplayer.Shared;
 using GrandTheftMultiplayer.Shared.Math;
-
-public class Cars : Script
+//Dieses Script darf nacher nur unter Admins laufen.
+public class Cars : Script 
 {
     [Command("veh", Alias = "Vehicle")]//Command führt aus das /veh oder /vehicle das Auto Gespawnt wird.
     public void veh(Client player, string vehicle, int color1 = 0, int color2 = 0) //int Hash = Deklaration des namens
@@ -15,7 +15,7 @@ public class Cars : Script
         VehicleHash myVehicle = API.vehicleNameToModel(vehicle); //Spawnt durch string vehicle jegliches model was ich will.
         if (myVehicle != ((VehicleHash)0))
         {
-            Vector3 vehPos = player.position; //die Definition 
+            Vector3 vehPos = player.position; //die Definition der x Axe für Player!
             Vector3 vehRot = player.rotation;
             if(color1 > 159) { color1 = 0; } //Größer als 159 erstellt 0 = Schwarz
             if(color2 > 159) { color2 = 0; }
