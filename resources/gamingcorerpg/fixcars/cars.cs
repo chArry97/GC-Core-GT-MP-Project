436409145cb07451a1d2760f9b34cabec9c5b30e
Unknown <fussballer01@googlemail.com>
2017-12-30 03:38:57 +0100

    VHH

--- resources/gamingcorerpg/fixcars/cars.cs
+++ resources/gamingcorerpg/fixcars/cars.cs
@@ -19,7 +19,7 @@ public class Cars : Script
             Vector3 vehRot = player.rotation;
             if(color1 > 159) { color1 = 0; } //Größer als 159 erstellt 0 = Schwarz
             if(color2 > 159) { color2 = 0; }
-            API.createVehicle(VehicleHash.Adder, vehPos, new Vector3(0,0,vehRot.Z), color1, color2);
+            API.createVehicle(myvehicle, vehPos, new Vector3(0,0,vehRot.Z), color1, color2);
             
             API.sendChatMessageToPlayer(player, "Dein Auto wurde gespawnt.");
         }
@@ -27,6 +27,19 @@ public class Cars : Script
         {
             API.sendChatMessageToPlayer(player, "Dein Auto wurde nicht gefunden.");
         }
+        
+    }
+    [Command("vehh", Alias = "HashVehicle")]//Command führt aus das /veh oder /vehicle das Auto Gespawnt wird.
+    public void vehh(Client player, int vehicle, int color1 = 0, int color2 = 0) //int Hash = Deklaration des namens
+    {
+
+            Vector3 vehPos = player.position; //die Definition 
+            Vector3 vehRot = player.rotation;
+            if (color1 > 159) { color1 = 0; } //Größer als 159 erstellt 0 = Schwarz
+            if (color2 > 159) { color2 = 0; }
+            API.createVehicle((VehicleHash) vehicle, vehPos, new Vector3(0, 0, vehRot.Z), color1, color2);
+
+            API.sendChatMessageToPlayer(player, "Dein Auto wurde gespawnt.");
         
     }
 }
