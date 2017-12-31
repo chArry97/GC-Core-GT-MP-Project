e06ac3b932941fbda106ad29d1db29d73f52b4cc
André <andre.kirsch@t-online.de>
2017-12-31 02:58:59 +0100

    Spieler speichert zu Spielbeginn Adminlevel auf dem Server

--- resources/gamingcorerpg/login/PlayerConnection.cs
+++ resources/gamingcorerpg/login/PlayerConnection.cs
@@ -46,7 +46,7 @@ public class PlayerConnection : Script {
 					
 					if (arguments[1].Equals(PlayerConnection.Base64Decode(dbpw))) {
 						player.sendNotification ("Login", "Einloggen erfolgreich");
-						loginPlayerSuccess(player);
+						loginPlayerSuccess(player, arguments[0]);
 					} else {
 						player.sendNotification ("Login Error", "Email oder Passwort falsch");
 					}
@@ -78,7 +78,7 @@ public class PlayerConnection : Script {
 					cmd.Parameters.AddWithValue("@password", PlayerConnection.Base64Encode((string) arguments[1]));
 
 					cmd.ExecuteNonQuery();
-					loginPlayerSuccess(player);
+					loginPlayerSuccess(player, arguments[0]);
 				} else {
 					player.sendNotification ("Register Error", "Ein Account mit dieser Email ist schon vorhanden!");
 				}
@@ -101,7 +101,27 @@ public class PlayerConnection : Script {
 		return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
 	}
 	
-	private void loginPlayerSuccess(Client player) {
+	private void loginPlayerSuccess(Client player, string email) {
+		
+		MySqlConnection conn = Database.getDatabase();
+		try {
+			conn.Open();
+			MySqlCommand cmd = conn.CreateCommand();
+			cmd.CommandText = "SELECT Adminlevel FROM user WHERE Email = @email";
+			cmd.Parameters.AddWithValue("@email", email);
+			
+			DataTable result = new DataTable();
+			result.Load(cmd.ExecuteReader());		
+			Int32 adminlevel = Int32.Parse((string) result.Rows[0]["Adminlevel"]);
+			
+			API.SetEntityData(player.handle, "Adminlevel", adminlevel);
+			
+			conn.Close();
+		} catch (MySqlException) {
+			API.consoleOutput ("ERROR connecting to database failed");
+			player.sendNotification ("Login Error", "Informationen über Gruppenstatus konnte nicht abgerufen werden!");
+		}
+		
 		player.nametagVisible = true;
 		player.invincible = false;
 		player.position = new Vector3(-1370.6250, 56.1227, 0);
